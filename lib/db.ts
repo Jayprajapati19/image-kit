a3a5c2d0f09c23bfba114b1d4545b29132ba7cec
Jayprajapati19 <jayp37679@gmail.com>
2025-02-08 17:34:10 +0530

    type script mongo connection

---
+++ lib/db.ts
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+
+const MOGODB_URI = process.env.MONGODB_URI!;
+
+if (!MOGODB_URI) {
+    throw new Error("check your database connection string");
+}
+
+
+let cached = global.mongoose;
+
+
+if (!cached) {
+    cached = global.mongoose = {
+        conn: null,
+        promise: null
+    }
+}
+
+
+export async function connectToDatabase() {
+    if (cached.conn) {
+        return cached.conn;
+    }
+
+    if (!cached.promise) {
+        const opts = {
+            bufferCommands: true,
+            maxPoolSize: 10
+        };
+        cached.promise = mongoose
+            .connect(MOGODB_URI, opts).then()
+    }
+}
